e78744393b006be280240edec9938d400c82f457
Joshua Grierson <jgrierson101@gmail.com>
2022-08-18 18:54:28 +0100

    [dockerfile]

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+ARG FUNCTION_DIR="/function"
+
+FROM python:3.7 as build-image
+
+RUN apt-get update && \
+    apt-get install -y \
+    g++ \
+    make \
+    cmake \
+    unzip \
+    libcurl4-openssl-dev
+
+ARG FUNCTION_DIR
+
+RUN mkdir -p ${FUNCTION_DIR}
+
+COPY app.py ${FUNCTION_DIR}
+
+RUN curl -L -o ${FUNCTION_DIR}/models/models.pbmm https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm
+RUN curl -L -o ${FUNCTION_DIR}/models/models.scorer https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer
+
+# Lambda runtime initerface client
+RUN pip install \
+    --target ${FUNCTION_DIR} \
+    awslambdaric
+
+FROM python:3.7
+
+ARG FUNCTION_DIR
+
+WORKDIR ${FUNCTION_DIR}
+
+COPY --from=build-image ${FUNCTION_DIR} ${FUNCTION_DIR}
+
+ADD https://github.com/aws/aws-lambda-runtime-interface-emulator/releases/latest/download/aws-lambda-rie /usr/bin/aws-lambda-rie
+
+RUN chmod 755 /usr/bin/aws-lambda-rie
+RUN pip install deepspeech
+
+ENTRYPOINT [ "/usr/bin/aws-lambda-rie", "/usr/local/bin/python", "-m", "awslambdaric" ]
+
+CMD [ "app.lambda_handler" ]
---
+++ app.py
@@ -0,0 +1,74 @@
+from deepspeech import Model
+import numpy as np
+import json
+
+modelPath = "./models/models.pbmm"
+scorerPath = "./models/models.scorer"
+
+def words_from_candidate_transcript(metadata):
+    word = ""
+    word_list = []
+    word_start_time = 0
+    # Loop through each character
+    for i, token in enumerate(metadata.tokens):
+        # Append character to word if it's not a space
+        if token.text != " ":
+            if len(word) == 0:
+                # Log the start time of the new word
+                word_start_time = token.start_time
+
+            word = word + token.text
+        # Word boundary is either a space or the last character in the array
+        if token.text == " " or i == len(metadata.tokens) - 1:
+            word_duration = token.start_time - word_start_time
+
+            if word_duration < 0:
+                word_duration = 0
+
+            each_word = dict()
+            each_word["word"] = word
+            each_word["start_time"] = round(word_start_time, 4)
+            each_word["duration"] = round(word_duration, 4)
+
+            word_list.append(each_word)
+            # Reset
+            word = ""
+            word_start_time = 0
+
+    return word_list
+
+
+def metadata_json_output(metadata):
+    json_result = dict()
+    json_result["transcripts"] = [{
+        "confidence": transcript.confidence,
+        "words": words_from_candidate_transcript(transcript),
+    } for transcript in metadata.transcripts]
+    return json.dumps(json_result, indent=None)
+
+def fetch_audio_file():
+    file = open("./audio/test3.wav", "rb")
+    audio = np.frombuffer(file.read(), dtype=np.int16)
+    return audio
+
+# TODO: Transcode audio to have same sample rate as DS using Sox
+def lambda_handler(event, context):
+    ds = Model(modelPath)
+    statusCode = 200
+    response = None
+    print(event)
+    try:
+        ds.enableExternalScorer(scorerPath)
+        samplerate = ds.sampleRate()
+        print("Deepspeech sample rate " + str(samplerate))
+        audio = fetch_audio_file()
+        response = metadata_json_output(ds.sttWithMetadata(audio, 2))
+    except Exception as ex:
+        print("Error occurred attempting to transcribe")
+        print(ex)
+        statusCode = 400
+        response = "TRANSCRIBE_FAILED"
+    return {
+        "statusCode": statusCode,
+        "body": response
+    }
